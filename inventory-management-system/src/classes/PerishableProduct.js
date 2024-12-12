0a520abdf5d00032eb592627cfc64bf2d65d39e6
H Joseph <hjoseph777@gmail.com>
2024-12-12 18:30:33 -0500

    main

--- inventory-management-system/src/classes/PerishableProduct.js
+++ inventory-management-system/src/classes/PerishableProduct.js
@@ -3,6 +3,9 @@ import ProductProperties from './Product.js';
 class PerishableProductProperties extends ProductProperties {
     constructor(name, price, quantity, expirationDate) {
         super(name, price, quantity);
+        if (!expirationDate || isNaN(Date.parse(expirationDate))) {
+            throw new Error('Invalid expiration date');
+        }
         this.expirationDate = expirationDate;
     }
 
